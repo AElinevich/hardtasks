7dd5811d64be5a1368c6ee12eb799bffa1d8a6f7
AElinevich <a.elinevich@tut.by>
2020-06-09 19:15:13 +0300

    done hardtask03

--- script/script.js
+++ script/script.js
@@ -1,39 +1,11 @@
-let lang = prompt('What is your language(en/ru)?');
-let EngDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-let RuDays = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 
-// Task 1(а)
-if (lang === 'ru') {
-    console.log(RuDays);
-} else if (lang === 'en') {
-    console.log(EngDays);
-} 
-
-// Task 1(b)
-switch (lang) {
-    case 'ru':
-    console.log(RuDays);
-    break;
-    case 'en':
-    console.log(EngDays);   
-    break;
-} 
-
-// Task 1(c)
-let allLanguages = [
-    ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-    ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
-
-];
-let result = 
-    (lang === 'en') ? console.log(allLanguages[0]) :
-    (lang === 'ru') ? console.log(allLanguages[1]) :
-    console.log('Wrong answer');
-
-// Task 2
-let namePerson = prompt('Как тебя зовут?', '');
-
-let message = 
-    (namePerson === 'Артем') ? console.log('Директор') : 
-    (namePerson === 'Максим') ?  console.log('Преподаватель') :
-    console.log('Студент');
+function message(a) {
+if (typeof a != 'string') {
+    return ('Not string');
+} else if (typeof a === 'string') {
+let str = a.trim();
+    if (str.length > 30) {
+        return str.replace(str.slice(30, str.length), '...');
+}
+}
+}
