05be86e752f7c8fe967ef817ad0325b1b7672aa2
AElinevich <a.elinevich@tut.by>
2020-06-10 10:36:31 +0300

    new new if

--- script/script.js
+++ script/script.js
@@ -3,9 +3,11 @@ function message(a) {
 if (typeof a != 'string') {
     return ('Not string');
 } else if (typeof a === 'string') {
-let str = a.trim();
+    let str = a.trim();
+    
     if (str.length > 30) {
-        return str.replace(str.slice(30, str.length), '...');
+        return str.replace(str.slice(31, str.length), '...');
 }
+    if (str.length <= 30) return "It's string"
 }
 }
