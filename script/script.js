d75061101439d30b27f662b8ef8603ef961ca8db
AElinevich <a.elinevich@tut.by>
2020-06-10 17:21:52 +0300

    done hardtask04

--- script/script.js
+++ script/script.js
@@ -1,13 +1,20 @@
+// 1
+let arr = ['2000', '65000', '9593952', '42383859','2575849', '9383', '6423', '4354'];
 
-function message(a) {
-if (typeof a != 'string') {
-    return ('Not string');
-} else if (typeof a === 'string') {
-    let str = a.trim();
-    
-    if (str.length > 30) {
-        return str.replace(str.slice(31, str.length), '...');
-}
-    if (str.length <= 30) return "It's string"
-}
-}
+for (let i = 0; i < arr.length; i++) {
+   if (arr[i][0] == 2 || arr[i][0] == 4) {
+     console.log(arr[i])
+   };
+};
+
+// 2
+
+   let x = 100;
+   
+   searcNumber:
+   for (let i = 2; i <= x; i++) {
+       for (let k = 2; k < i; k++) {
+           if(i % k == 0) continue searcNumber;
+       }
+       console.log(i + '-' + 'Делители этого числа: ' + i + ' и 1')
+   }
