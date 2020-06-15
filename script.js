20b29e95634141669bebd0637cc14090028ced5f
AElinevich <58183312+AElinevich@users.noreply.github.com>
2020-06-15 11:20:53 +0300

    correct mistakes

---
+++ script.js
@@ -0,0 +1,37 @@
+// 1
+let currentDate = new Date;
+let indexCurrent = currentDate.getUTCDay();
+let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг','Пятница', 'Суббота', 'Воскресенье'];
+console.log(week);
+console.log(indexCurrent);
+
+
+
+week.forEach(function(item, i){
+  let list = document.createElement('div');
+ console.log();
+    list.innerHTML =  `
+      <ul>
+        <li>${item}</li>
+     </ul>
+  `;
+
+  document.body.insertAdjacentElement('beforeend', list);
+
+  if(item === 'Суббота' || item === 'Воскресенье') {
+    list.style.fontStyle = "italic";
+  }
+  if(i + 1  ===  indexCurrent || i - 6 === indexCurrent) {
+    list.style.fontWeight = "bold";
+
+  };
+  // if() {
+  //   list.style.fontWeight = "bold";
+
+  // };
+});
+
+
+
+
+
