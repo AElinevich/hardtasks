2629bcb78098422114cade94578141b0ae587557
AElinevich <a.elinevich@tut.by>
2020-06-16 22:19:53 +0300

    done hardtask09

--- script/script.js
+++ script/script.js
@@ -1,24 +1,25 @@
-// 1
+
 let currentDate = new Date;
 let indexCurrent = currentDate.getUTCDay();
 let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг','Пятница', 'Суббота', 'Воскресенье'];
-
+console.log(week);
+console.log(indexCurrent);
 
 week.forEach(function(item, i){
   let list = document.createElement('div');
- 
+ console.log();
     list.innerHTML =  `
       <ul>
         <li>${item}</li>
      </ul>
   `;
 
-  document.body.insertAdjacentElement('beforeend', list);
-
+document.body.insertAdjacentElement('beforeend', list);
+// list.style.listStyleType = "none";
   if(item === 'Суббота' || item === 'Воскресенье') {
     list.style.fontStyle = "italic";
   }
-  if(i  ===  indexCurrent + 6) {
+  if(i + 1  ===  indexCurrent || i - 6 === indexCurrent) {
     list.style.fontWeight = "bold";
 
   };
