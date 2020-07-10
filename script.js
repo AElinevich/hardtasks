667869aadf30effc1944e316f7f49304899cd533
AElinevich <a.elinevich@tut.by>
2020-07-10 14:12:07 +0300

    correct hardtask23

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ let task2 = document.getElementById('task2');
 task2.innerHTML = task2.textContent.replace( /\b\d\d:\d\d\b/g , match => `<b>${match}</b>`);
 
 // 3
-document.body.innerHTML = document.body.textContent.replace( /"[^"]+"/g, match => `<mark>${match}</mark>`);
+document.body.innerHTML = document.body.textContent.replace( /("|«)[^("|»)]+("|»)/g, match => `<mark>${match}</mark>`);
 
-// 4 
-let http = document.body.textContent.match(/ http:.*? /g);
-
-let site = http.toString().match(/\w+\.\w{2,3}/g);
-
-document.body.innerHTML = document.body.textContent.replace(/ http:.*? /g, match => `<a href="${match}">${site[1]}</a>`)
+//4 
+document.body.innerHTML = document.body.textContent.replace(/(\w+\.)+\w+/g, match => `<a href="${match}">${match}</a>`)
  
-// 5
-let color = document.body.textContent.match( /#ABCDEF/ );
-console.log(color);
+
+//5
+let color = document.body.textContent.match( /#[a-f0-9]{6}/gi);
+
+
+
+
